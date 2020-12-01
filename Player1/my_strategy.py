b2e3ee8efd0a96d715dccfff26fbab29dae3045a
Dmitry Biryukov <bidmal@mail.ru>
2020-12-01 15:11:45 +0300

    refactored

--- Player1/my_strategy.py
+++ Player1/my_strategy.py
@@ -1,147 +1,158 @@
-import model
+# import model.
+from model import EntityType, Vec2Int, EntityAction, Action, BuildAction, MoveAction, AttackAction, DebugCommand
 
-WALL = 0
-HOUSE = 1
-BUILDER_BASE = 2
-BUILDER_UNIT = 3
-MELEE_BASE = 4
-MELEE_UNIT = 5
-RANGED_BASE = 6
-RANGED_UNIT = 7
-RESOURCE = 8
-TURRET = 9
+
+class Calc:
+
+    @staticmethod
+    def distance_sqr(a, b):
+        return (a.x - b.x) ** 2 + (a.y - b.y) ** 2
+
+    @staticmethod
+    def sign(a):
+        result = None
+        if a > 0:
+            result = 1
+        elif a < 0:
+            result = -1
+        else:
+            result = 0
+        return result
+
+    @staticmethod
+    def find_closest(entity, targets):
+        return
+
+
+class Game:
+
+    def __init__(self, map_size, my_id, players):
+
+        self.map_size = map_size
+        self.my_id = my_id
+        self.enemy_ids = []
+        self.my_resource_count = None
+        for player in players:
+            if player.id == my_id:
+                self.my_resource_count = player.resource
+            else:
+                self.enemy_ids.append(player.id)
+
+    def parse_entities(self, entities):
+        self.my_walls = []
+        self.my_houses = []
+        self.my_builder_bases = []
+        self.my_builder_units = []
+        self.my_melee_bases = []
+        self.my_melee_units = []
+        self.my_ranged_bases = []
+        self.my_ranged_units = []
+        self.resources = []
+        self.res_avails = {}
+        self.my_turrets = []
+        self.enemy_units = []
+        self.enemy_buildings = []
+
+        for entity in entities:
+            if entity.entity_type == EntityType.RESOURCE:
+                self.resources.append(entity)
+                self.res_avails[entity.id] = True
+            if entity.player_id == self.my_id:
+                if entity.entity_type == EntityType.WALL:
+                    self.my_walls.append(entity)
+                elif entity.entity_type == EntityType.HOUSE:
+                    self.my_houses.append(entity)
+                elif entity.entity_type == EntityType.BUILDER_BASE:
+                    self.my_builder_bases.append(entity)
+                elif entity.entity_type == EntityType.BUILDER_UNIT:
+                    self.my_builder_units.append(entity)
+                elif entity.entity_type == EntityType.MELEE_BASE:
+                    self.my_melee_bases.append(entity)
+                elif entity.entity_type == EntityType.MELEE_UNIT:
+                    self.my_melee_units.append(entity)
+                elif entity.entity_type == EntityType.RANGED_BASE:
+                    self.my_ranged_bases.append(entity)
+                elif entity.entity_type == EntityType.RANGED_UNIT:
+                    self.my_ranged_units.append(entity)
+                elif entity.entity_type == EntityType.TURRET:
+                    self.my_turrets.append(entity)
+            else:
+                if entity.entity_type in [EntityType.MELEE_UNIT, EntityType.RANGED_UNIT, EntityType.TURRET, EntityType.BUILDER_UNIT]:
+                    self.enemy_units.append(entity)
+                elif entity.entity_type in [EntityType.WALL, EntityType.HOUSE, EntityType.BUILDER_BASE, EntityType.MELEE_BASE, EntityType.RANGED_BASE]:
+                    self.enemy_buildings.append(entity)
 
 
 class MyStrategy:
 
     def get_action(self, player_view, debug_interface):
 
-        max_dist = player_view.map_size**2
-        my_id = player_view.my_id
-        my_resources = None
-        enemies = []
-        my_walls = []
-        my_houses = []
-        my_builder_bases = []
-        my_builder_units = []
-        my_melee_bases = []
-        my_melee_units = []
-        my_ranged_bases = []
-        my_ranged_units = []
-        resources = []
-        res_avails = {}
-        my_turrets = []
-        enemy_units = []
-        enemy_buildings = []
         entity_actions = {}
+        game = Game(player_view.map_size, player_view.my_id, player_view.players)
+        game.parse_entities(player_view.entities)
 
-        for player in player_view.players:
-            if player.id == my_id:
-                my_resources = player.resource
-            else:
-                enemies.append(player.id)
-
-        for entity in player_view.entities:
-            if entity.entity_type == RESOURCE:
-                resources.append(entity)
-                res_avails[entity.id] = True
-            if entity.player_id == my_id:
-                if entity.entity_type == WALL:
-                    my_walls.append(entity)
-                elif entity.entity_type == HOUSE:
-                    my_houses.append(entity)
-                elif entity.entity_type == BUILDER_BASE:
-                    my_builder_bases.append(entity)
-                elif entity.entity_type == BUILDER_UNIT:
-                    my_builder_units.append(entity)
-                elif entity.entity_type == MELEE_BASE:
-                    my_melee_bases.append(entity)
-                elif entity.entity_type == MELEE_UNIT:
-                    my_melee_units.append(entity)
-                elif entity.entity_type == RANGED_BASE:
-                    my_ranged_bases.append(entity)
-                elif entity.entity_type == RANGED_UNIT:
-                    my_ranged_units.append(entity)
-                elif entity.entity_type == TURRET:
-                    my_turrets.append(entity)
-            else:
-                if entity.entity_type in [MELEE_UNIT, RANGED_UNIT, TURRET, BUILDER_UNIT]:
-                    enemy_units.append(entity)
-                elif entity.entity_type in [WALL, HOUSE, BUILDER_BASE, MELEE_BASE, RANGED_BASE]:
-                    enemy_buildings.append(entity)
-
-        my_unit_count = len(my_builder_units) + \
-            len(my_melee_units) + len(my_ranged_units)
-        my_food_count = 15 + 5*len(my_houses)
+        my_unit_count = len(game.my_builder_units) + len(game.my_melee_units) + len(game.my_ranged_units)
+        my_food_count = 15 + 5*len(game.my_houses)
         my_unit_slots = my_food_count - my_unit_count
-        my_army = my_melee_units + my_ranged_units
+        my_army = game.my_melee_units + game.my_ranged_units
 
-        for artillery in my_ranged_bases:
-            buildAction = None
-            if my_unit_slots and my_resources > 29:
-                position = model.Vec2Int(
-                    artillery.position.x-1, artillery.position.y)
-                buildAction = model.BuildAction(
-                    model.EntityType.BUILDER_UNIT, position)
-            entity_actions[artillery.id] = model.EntityAction(
-                None, buildAction, None, None)
+        for my_ranged_base in game.my_ranged_bases:
+            build_action = None
+            if my_unit_slots and game.my_resource_count > 29:
+                position = Vec2Int(my_ranged_base.position.x-1, my_ranged_base.position.y)
+                build_action = BuildAction(EntityType.BUILDER_UNIT, position)
+            entity_actions[my_ranged_base.id] = EntityAction(
+                None, build_action, None, None)
 
-        for base in my_builder_bases:
-            buildAction = None
-            if my_unit_slots and my_resources > 9:
-                position = model.Vec2Int(
-                    base.position.x-1, base.position.y)
-                buildAction = model.BuildAction(
-                    model.EntityType.BUILDER_UNIT, position)
-            entity_actions[base.id] = model.EntityAction(
-                None, buildAction, None, None)
+        for my_builder_base in game.my_builder_bases:
+            build_action = None
+            if my_unit_slots and game.my_resource_count > 9:
+                position = Vec2Int(my_builder_base.position.x-1, my_builder_base.position.y)
+                build_action = BuildAction(EntityType.BUILDER_UNIT, position)
+            entity_actions[my_builder_base.id] = EntityAction(None, build_action, None, None)
 
         for ship in my_army:
             cur_pos = ship.position
-            moveAction = None
-            attackAction = None
-            dist = max_dist
+            move_action = None
+            attack_action = None
+            dist = game.map_size**2
             move_target = None
             attack_target = None
             if len(my_army) < 6:
-                move_target = model.Vec2Int(
-                    my_ranged_bases[0].position.x+4, my_ranged_bases[0].position.y+4)
+                move_target = Vec2Int(game.my_ranged_bases[0].position.x+4, game.my_ranged_bases[0].position.y+4)
             else:
-                if len(enemy_units) > 0:
-                    for unit in enemy_units:
-                        cur_dist = (cur_pos.x - unit.position.x)**2 + \
-                            (cur_pos.y - unit.position.y)**2
+                if len(game.enemy_units) > 0:
+                    for unit in game.enemy_units:
+                        cur_dist = (cur_pos.x - unit.position.x)**2 + (cur_pos.y - unit.position.y)**2
                         if cur_dist < dist:
                             dist = cur_dist
                             move_target = unit.position
                             attack_target = unit.id
                             if dist < 2:
                                 break
-                elif len(enemy_buildings) > 0:
-                    for building in enemy_buildings:
-                        cur_dist = (cur_pos.x - building.position.x)**2 + \
-                            (cur_pos.y - building.position.y)**2
+                elif len(game.enemy_buildings) > 0:
+                    for building in game.enemy_buildings:
+                        cur_dist = (cur_pos.x - building.position.x)**2 + (cur_pos.y - building.position.y)**2
                         if cur_dist < dist:
                             dist = cur_dist
                             move_target = building.position
                             attack_target = building.id
                             if dist < 2:
                                 break
-            moveAction = model.MoveAction(move_target, True, False)
-            attackAction = model.AttackAction(attack_target, None)
-            entity_actions[ship.id] = model.EntityAction(
-                moveAction, None, attackAction, None)
+            move_action = MoveAction(move_target, True, False)
+            attack_action = AttackAction(attack_target, None)
+            entity_actions[ship.id] = EntityAction(
+                move_action, None, attack_action, None)
 
-        for builder in my_builder_units:
+        for builder in game.my_builder_units:
             cur_pos = builder.position
             target_res = None
-            moveAction = None
-            attackAction = None
-            dist = max_dist
-            for res in resources:
-                if res_avails[res.id]:
-                    cur_dist = (cur_pos.x - res.position.x)**2 + \
-                        (cur_pos.y - res.position.y)**2
+            move_action = None
+            attack_action = None
+            dist = game.map_size**2
+            for res in game.resources:
+                if game.res_avails[res.id]:
+                    cur_dist = (cur_pos.x - res.position.x)**2 + (cur_pos.y - res.position.y)**2
 
                     if cur_dist < dist:
                         dist = cur_dist
@@ -149,15 +160,15 @@ class MyStrategy:
                         target_position = res.position
                         if dist < 2:
                             break
-            res_avails[target_res] = False
-            moveAction = model.MoveAction(target_position, True, False)
-            attackAction = model.AttackAction(target_res, None)
-            entity_actions[builder.id] = model.EntityAction(
-                moveAction, None, attackAction, None)
+            game.res_avails[target_res] = False
+            move_action = MoveAction(target_position, True, False)
+            attack_action = AttackAction(target_res, None)
+            entity_actions[builder.id] = EntityAction(
+                move_action, None, attack_action, None)
 
-        # print("entity_actions: {}".format(entity_actions))
-        return model.Action(entity_actions)
+        # print(f"entity_actions: {entity_actions}")
+        return Action(entity_actions)
 
     def debug_update(self, player_view, debug_interface):
-        debug_interface.send(model.DebugCommand.Clear())
+        debug_interface.send(DebugCommand.Clear())
         debug_interface.get_state()
